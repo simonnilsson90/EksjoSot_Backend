195ef14db90a2db4d44c9eb42ca7b4d5a436d7c0
William Karlström <williamcarlstrom1@gmail.com>
2024-04-24 09:54:43 +0200

    Fix webcfg annotation

--- src/main/java/Heroku/Project/Config/WebConfig.java
+++ src/main/java/Heroku/Project/Config/WebConfig.java
@@ -1,7 +1,8 @@
-package Heroku.Project;
+package Heroku.Project.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,11 +13,11 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("/*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
+                        .allowedHeaders("/*")
                         .allowCredentials(true);
             }
         };
